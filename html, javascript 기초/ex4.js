64c0a1acc5793c8182f5fbd5950eb991b47bd0af
umyunsang <dbstkd5865@gmail.com>
2024-09-20 12:13:02 +0900

    2024-09-20

--- html, javascript 기초/ex4.js
+++ html, javascript 기초/ex4.js
@@ -1,10 +1,13 @@
 let n, sum;
 
 n = prompt("숫자를 입력하세요.");
-document.write("n ==> " + n + "<hr>");
+// document.write("n ==> " + n + "<hr>");
+document.write(`n ==>  ${n}  <hr>`);
 
 sum = n + 100;
-document.write("n + 100 ==> " + sum + "<hr>");
+// document.write("n + 100 ==> " + sum + "<hr>");
+document.write(`n + 100 ==>  ${sum}  <hr>`);
 
 sum = parseInt(n) + 100;
-document.write("정수변환n + 100 ==> " + sum + "<hr>");
+// document.write("정수변환n + 100 ==> " + sum + "<hr>");
+document.write(`정수변환n + 100 ==>  ${sum}  <hr>`);
--- html, javascript 기초/ex5.js
+++ html, javascript 기초/ex5.js
@@ -1,20 +1,25 @@
 function changeColor() {
     const co = document.getElementById("co").value;
+ 
   
     // 입력된 색상을 body의 글자색으로 설정
-    document.body.style.color = co;
+    document.body.style.backgroundColor = co;
+    document.getElementById("co").style.color = co; 
   
     // 입력된 색상이 잘못되었을 경우 팝업창
-    if (!isValidColor(co)) {
-      alert(co + "는 존재하지 않는 색입니다. 다시 작성해 주세요 ^^");
+    if (co == "") {
+        alert("색을 입력해 주세요"); return
     }
+    else if (!isValidColor(co)) {
+      alert(co + "는 존재하지 않는 색입니다. 다시 작성해 주세요 ^^"); return
+    }
+
   }
   
   function isValidColor(color) {
     // 유효한 색상인지 확인
     const st = new Option().style;
     st.color = color;
-    if (st.color == "") return false;
-    else return true;
+    return st.color != "";
   }
   
