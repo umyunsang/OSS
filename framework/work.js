6af7ecb4a1698d1a6bafe36499c0a5a1853d697d
umyunsang <dbstkd5865@gmail.com>
2024-12-09 11:27:06 +0900

    12-09

---
+++ framework/work.js
@@ -0,0 +1,16 @@
+// words.js
+const dictionary = {
+    "사과": "apple",
+    "바나나": "banana",
+    "포도": "grape",
+    "오렌지": "orange",
+    "수박": "watermelon",
+    "체리": "cherry",
+    "키위": "kiwi",
+    "복숭아": "peach",
+    "레몬": "lemon",
+    "망고": "mango",
+  };
+  
+  export default dictionary;
+  
